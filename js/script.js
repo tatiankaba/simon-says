2749b45fb3dbdfedafb76c9a13978af84d5def34
Tatiana <bodtata@yandex.ru>
2025-01-12 18:35:44 +0100

    feat: create start page

--- js/script.js
+++ js/script.js
@@ -1,18 +1,263 @@
-import 'domElementsCreation.js'
-import 'startPage.js'
-
 'use strict'
 
 document.addEventListener('DOMContentLoaded', ()=> {
 
-  const body = document.querySelector('body');
+  //сreate dom elements 
 
-  function createStartPage(appendTo) {
-    const gameContainer = document.createElement('div');
-    gameContainer.classList.add('container');
+  function createStartBtn() {
+    const startBtn = document.createElement('button');
+    startBtn.classList.add('btn');
+    startBtn.classList.add('start-btn');
+    startBtn.textContent = 'start';
+    return startBtn;
+  }
+
+  function createNewGameBtn(appendTo, level) {
+    const newGameBtn = document.createElement('button');
+    newGameBtn.classList.add('btn');
+    newGameBtn.classList.add('new-game-btn');
+    newGameBtn.textContent = 'new game';
+
+    newGameBtn.addEventListener('click', ()=> {
+      appendTo.innerHTML = '';
+      createStartWindow(appendTo, level)
+    })
+    return newGameBtn
+  }
+
+  function createRepeatTheSequency() {
+    const repeatTheSequencyBtn = document.createElement('button');
+    repeatTheSequencyBtn.classList.add('btn');
+    repeatTheSequencyBtn.classList.add('repeat-the-sequency-btn');
+    repeatTheSequencyBtn.textContent = 'repeat the sequency';
+    return repeatTheSequencyBtn;
+  }
+
+  function createNextBtn() {
+    const nextBtn = document.createElement('button');
+    nextBtn.classList.add('btn');
+    nextBtn.classList.add('next-btn');
+    nextBtn.textContent = 'next';
+    return nextBtn
+  }
+
+  function createRoundInput() {
+    const labelForRoundInput = document.createElement('label');
+    labelForRoundInput.classList.add('label-for-roundInput');
+    labelForRoundInput.textContent = 'Round:';
+    const roundInput = document.createElement('input');
+    roundInput.type = 'number';
+    roundInput.value = 1;
+    roundInput.classList.add('round-input');
+    roundInput.setAttribute('disabled', '');
+    roundInput.setAttribute('id', 'round-input');
+    labelForRoundInput.setAttribute('for', 'round-input');
+    labelForRoundInput.append(roundInput);
+    return labelForRoundInput;
+  }
+
+  function createSequencyInput(appendTo) {
+    const labelForSequencyInput = document.createElement('label');
+    labelForSequencyInput.classList.add('label-for-sequencyInput');
+    labelForSequencyInput.textContent = 'Your sequency:';
+    const sequencyInput = document.createElement('input');
+    sequencyInput.classList.add('sequency-input');
+    sequencyInput.type = 'text'
+    sequencyInput.setAttribute('disabled', '');
+    labelForSequencyInput.setAttribute('for', 'sequency-input' )
+    sequencyInput.setAttribute('id', 'sequency-input');
+    labelForSequencyInput.append(sequencyInput);
+    return labelForSequencyInput;
+  }
+
+  function createVirtualKeyboardContainer() {
+    const virtualKeyboardContainer = document.createElement('div');
+    virtualKeyboardContainer.classList.add('keyboard-container');
+    return virtualKeyboardContainer;
+  }
+
+  function createVirtualNumberKeyboard() {
+    const numberBlock = document.createElement('div');
+    numberBlock.classList.add('number-keyboard');
+    const numberArray = '0123456789'.split('');
+    numberArray.forEach((number) => {
+      const num = document.createElement('div');
+      num.classList.add('key');
+      num.textContent = `${number}`;
+      numberBlock.append(num);
+    });
+    return numberBlock;
+  }
+
+  function createVirtualLettersKeyboard() {
+    const lettersBlock = document.createElement('div');
+    lettersBlock.classList.add('letters-keyboard');
+    const lettersArray = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+    lettersArray.forEach((letter) => {
+      const letterUnit = document.createElement('div');
+      letterUnit.classList.add('key');
+      letterUnit.textContent = `${letter.toUpperCase()}`;
+      lettersBlock.append(letterUnit);
+    });
+    return lettersBlock;
+  }
+
+  function createLevelsDropdownList() {
+    const label = document.createElement('label');
+    const levelInput = document.createElement('select');
+    const firstOption = document.createElement('option');
+    const secondOption = document.createElement('option');
+    const thirdOption = document.createElement('option');
+
+    label.setAttribute('for', 'level-dropdown-list ');
+    label.classList.add('label-for-levels');
+    label.textContent = 'Choose your level:';
+
+    levelInput.id = 'level-dropdown-list ';
+    levelInput.name = 'levels';
+
+    firstOption.value = 'easy';
+    firstOption.textContent = 'easy';
+    firstOption.setAttribute('selected', '');
+
+    secondOption.value = 'medium';
+    secondOption.textContent = 'medium';
+
+    thirdOption.value = 'hard';
+    thirdOption.textContent = 'hard';
+
+    label.append(levelInput);
+    levelInput.append(firstOption);
+    levelInput.append(secondOption);
+    levelInput.append(thirdOption);
+    return label;
   }
 
 
+  function createGameContainer() {
+    const body = document.querySelector('body');
+    const gameContainer = document.createElement('div');
+    gameContainer.classList.add('container');
+    body.append(gameContainer);
+  }
+
+  function createFeedbackBlock() {
+    const feedbackBlock = document.createElement('div');
+    feedbackBlock.classList.add('feedback-block');
+    feedbackBlock.textContent = 'Press Start to launch the game'
+    return  feedbackBlock;
+  }
+
+  //manipulate dom elements
+
+  function checkChosenLevel() {
+    const levelInput = document.querySelector('select');
+    return levelInput.value;
+}
+
+function removeStartBtn() {
+    const startBtn = document.querySelector('.start-btn');
+    startBtn.remove();
+}
+
+function listenLevelDropdownChange() {
+  const levelDropdown = document.querySelector('select')
+  levelDropdown.addEventListener('change', ()=> {
+    let chosenLevel = document.querySelector('select option:checked')
+    openLevel(chosenLevel);
+
+  })
+}
+
+ function changeFeedBackBlock(text) {
+    const feedbackBlock = document.querySelector('.feedback-block');
+    feedbackBlock.textContent = text;
+}
+
+function disableLevelDropdownList() {
+  const dropdownLevelList = document.querySelector('select');
+  dropdownLevelList.setAttribute('disabled', '')
+}
+
+function openLevel(level) {
+  const levelBlock = document.querySelector('.keyboard-container');
+  level = checkChosenLevel();
+      switch (level) {
+          case 'easy': 
+          levelBlock.innerHTML = '';
+          levelBlock.append(createVirtualNumberKeyboard());
+            break;
+          case 'medium':
+            levelBlock.innerHTML = '';
+            levelBlock.append(createVirtualLettersKeyboard());
+            break;
+          case 'hard':
+            levelBlock.innerHTML = '';
+            levelBlock.append(createVirtualNumberKeyboard());
+            levelBlock.append(createVirtualLettersKeyboard());
+            break;
+}
+}
+
+
+
+
+  createGameContainer();
+  const gameContainer = document.querySelector('.container');
+
+  createStartWindow(gameContainer, 'easy');
+
+  
+function createStartWindow(appendTo, level) {
+  const startManageGameBlock = document.createElement('div');
+  startManageGameBlock.classList.add('game-manage-block');
+
+  const startBtnBlock = document.createElement('div');
+  startBtnBlock.classList.add('start-btn-block');
+
+  const startLevelsBlock = document.createElement('div');
+  startLevelsBlock.classList.add('start-levels-block');
+
+  appendTo.append(startManageGameBlock);
+  startManageGameBlock.append(startBtnBlock)
+  startBtnBlock.append(createStartBtn());
+  startManageGameBlock.append(createLevelsDropdownList());
+
+  if(level !== "easy") {
+    const dropdownLevelList = document.querySelector('select');
+    dropdownLevelList.querySelector(`option[value = ${level}]`).setAttribute('selected', '')
+
+  }
+
+  appendTo.append(startLevelsBlock);
+  startLevelsBlock.append(createFeedbackBlock())
+  startLevelsBlock.append(createSequencyInput());
+  startLevelsBlock.append(createVirtualKeyboardContainer());
+
+  openLevel(level);
+  listenLevelDropdownChange();
+
+  const startBtn = document.querySelector('.start-btn');
+  startBtn.addEventListener('click', ()=> {
+    changeFeedBackBlock('Simon says:');
+    removeStartBtn();
+    disableLevelDropdownList();
+    level = checkChosenLevel();
+    startBtnBlock.append(createNewGameBtn(appendTo, level));
+    startBtnBlock.append(createRepeatTheSequency());
+    startBtnBlock.append(createRoundInput());
+  
+
+
+  });
+  };
+
+
+
+
+  
+
+});
 
 
 
@@ -27,4 +272,4 @@ document.addEventListener('DOMContentLoaded', ()=> {
 
 
 
-})
+
