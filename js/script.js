bf429a581009588601209d52320144da4366e720
Tatiana <bodtata@yandex.ru>
2025-01-12 22:46:04 +0100

    feat: add interactivity to keys clicks

--- js/script.js
+++ js/script.js
@@ -2,6 +2,13 @@
 
 document.addEventListener('DOMContentLoaded', ()=> {
 
+  // game constants
+
+  let errorCounter = 0;
+  let pressCounter = 0;
+  let numberOfRounds = 5;
+  let roundCounter = 1;
+
   //сreate dom elements 
 
   function createStartBtn() {
@@ -150,6 +157,45 @@ document.addEventListener('DOMContentLoaded', ()=> {
 
   //manipulate dom elements
 
+  function changeSequenceInput(clickedKey) {
+    const sequenceInput = document.querySelector('.sequency-input');
+    sequenceInput.value += String(clickedKey);
+  }
+
+  function disableNewGameBtn() {
+    const newGameBtn = document.querySelector('.new-game-btn');
+    newGameBtn.disabled = true;
+  }
+
+  function enableNewGameBtn() {
+    const newGameBtn = document.querySelector('.new-game-btn');
+    newGameBtn.disabled = false;
+  }
+
+  function disableVirtualKeyboard() {
+    const keys = Array.from(document.getElementsByClassName('key'));
+    keys.forEach((key)=> {
+      key.style.pointerEvents = 'none'});
+  }
+  
+  function enableVirtualKeyboard() {
+    const keys = Array.from(document.getElementsByClassName('key'));
+    keys.forEach((key)=> {
+      key.style.pointerEvents = 'auto'});
+  }
+
+  function disableRepeatTheSequencyBtn() {
+    const repeatBtn = document.querySelector('.repeat-the-sequency-btn');
+    repeatBtn.disabled = true;
+  }
+
+  function enableRepeatTheSequencyBtn() {
+    const repeatBtn = document.querySelector('.repeat-the-sequency-btn');
+    repeatBtn.disabled = false;
+  }
+
+
+
   function checkChosenLevel() {
     const levelInput = document.querySelector('select');
     return levelInput.value;
@@ -196,18 +242,17 @@ function openLevel(level) {
             levelBlock.append(createVirtualNumberKeyboard());
             levelBlock.append(createVirtualLettersKeyboard());
             break;
-}
-}
+    }
+  };
+
+  function renewRoundCounterInput(roundCounter) {
+    const roundCounterInput = document.querySelector('.round-input');
+    roundCounterInput.value = roundCounter;
+  }
+
+// assemle main play page
 
 
-
-
-  createGameContainer();
-  const gameContainer = document.querySelector('.container');
-
-  createStartWindow(gameContainer, 'easy');
-
-  
 function createStartWindow(appendTo, level) {
   const startManageGameBlock = document.createElement('div');
   startManageGameBlock.classList.add('game-manage-block');
@@ -246,18 +291,129 @@ function createStartWindow(appendTo, level) {
     startBtnBlock.append(createNewGameBtn(appendTo, level));
     startBtnBlock.append(createRepeatTheSequency());
     startBtnBlock.append(createRoundInput());
-  
-
-
+    pressCounter = 0;
+    errorCounter = 0;
+    roundCounter = 1;
+    startGame(roundCounter);
   });
   };
 
 
+// laucnh game 
+
+createGameContainer();
+const gameContainer = document.querySelector('.container');
+createStartWindow(gameContainer, 'easy');
+
+// create round
+
+async function startGame(roundCounter = '1') {
+ 
+  const sequency = await createSequency(roundCounter);
+
+  const repeatSequency = async ()=> {
+    const repeatTheSequencyBtn = document.querySelector('.repeat-the-sequency-btn')
+      repeatTheSequencyBtn.disabled = true;
+      repeatTheSequencyBtn.classList.add('disabled')
+      await launchSequency(sequency);
+      disableRepeatTheSequencyBtn()
+      repeatTheSequencyBtn.removeEventListener('click', repeatSequency);
+  }
+  const repeatTheSequencyBtn = document.querySelector('.repeat-the-sequency-btn')
+  repeatTheSequencyBtn.addEventListener('click', repeatSequency);
+
+  await launchSequency(sequency);
+
+  let clickHandler = async (event)=> {
+    await new Promise((resolve, reject) => {
+      if (event.target.classList.contains('key')) {
+        let clickedKey = event.target.textContent.toUpperCase();
+        compareClickedKeysWithSequency(sequency, clickedKey);
+        resolve()
+      } else {
+        reject()
+      }
+    })
+  }
+  document.querySelector('.keyboard-container').addEventListener('click', clickHandler);
+
+  if(roundCounter > numberOfRounds) {
+    changeFeedBackBlock('You won the game');
+  }
+}
+
+async function launchSequency(sequency) {
+    const keys = Array.from(document.getElementsByClassName('key'));
+    disableVirtualKeyboard();
+    disableRepeatTheSequencyBtn();
+    disableNewGameBtn();
+    for (let sign of sequency) {
+      await new Promise((resolve) => {
+        const keyID = keys.findIndex(key => key.textContent.toUpperCase() === sign.toUpperCase()); 
+        setTimeout(()=>{
+          if (keyID !== -1) {
+            keys[keyID].classList.add('active');
+            setTimeout(() => {
+              keys[keyID].classList.remove('active');
+              resolve(); 
+            }, 3000);
+          } else {
+            resolve(); 
+          };
+        }, 0)
+      });
+    }
+    enableVirtualKeyboard();
+    enableRepeatTheSequencyBtn();
+    enableNewGameBtn();
+    changeFeedBackBlock('your turn:');
+}
 
 
-  
+function compareClickedKeysWithSequency(sequency, clickedKey){
+  if (clickedKey === sequency[pressCounter].toUpperCase()) {
+    pressCounter += 1;
+    changeFeedBackBlock('Continue:');
+    changeSequenceInput(clickedKey);
+    if(pressCounter === sequency.length) {
+      changeFeedBackBlock('you won the round');
+      renewRoundCounterInput(roundCounter+1);
+      //тут почитать требования добавить кнопку некст
+    }
+  } else {
+    errorCounter += 1;
+    changeFeedBackBlock('Mistake');
+    if(errorCounter > 1) {
+      changeFeedBackBlock('You lost the game');
+      'тут добавить то же самое'
+    }
+  }
+}
+
+async function createSequency(roundCounter) {
+  return new Promise((resolve) => {
+    const sequenceArray = [];
+    const keys = Array.from(document.getElementsByClassName('key'));
+    let keysID;
+    let randomKeysID = [];
+    for(let i = 0; i < (roundCounter * 2); i++) {
+      keysID =  Math.floor(Math.random() * keys.length);
+      randomKeysID.push(keysID);
+    }
+    randomKeysID.forEach((ID) => {
+      sequenceArray.push(keys[ID].textContent);
+    })
+    resolve(sequenceArray);
+  })
+}
+
+
+
+
+
 
 });
+
 
 
 
